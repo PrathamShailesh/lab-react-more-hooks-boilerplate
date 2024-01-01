930031da8cb2241939882b21de2bc6f4282eab0f
Pratham Shailesh Dsouza <prathamshailesh777@gmail.com>
2024-01-01 22:41:19 +0530

    new

---
+++ more_hooks/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Use from './component/UseState'
+import UseReducer from './component/UseReduce'
+
+function App() {
+
+  return (
+    <>
+    {/* <Use/> */}
+    <UseReducer/>
+
+    </>
+  )
+}
+
+export default App
